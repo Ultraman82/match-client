c9621546f1bf544b4ba74160efeab47ba1f35223
Ultrman82 <exelcior99@gmail.com>
2019-10-03 20:41:27 -0700

    deploy

---
+++ build/precache-manifest.a68250b3188749d50e619ce388c40d2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "15da6a1ded56d1afd8fe",
+    "url": "//match-client/static/css/main.7b2a3d2b.chunk.css"
+  },
+  {
+    "revision": "15da6a1ded56d1afd8fe",
+    "url": "//match-client/static/js/main.ec6c6d9f.chunk.js"
+  },
+  {
+    "revision": "0231e0a0077261206179",
+    "url": "//match-client/static/js/runtime~main.32835545.js"
+  },
+  {
+    "revision": "297ffedaa530ba866dda",
+    "url": "//match-client/static/css/2.121fc313.chunk.css"
+  },
+  {
+    "revision": "297ffedaa530ba866dda",
+    "url": "//match-client/static/js/2.df524de8.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "//match-client/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "//match-client/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "//match-client/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "//match-client/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "//match-client/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "92a096725ea900f26ddffa39eb9f02fa",
+    "url": "//match-client/index.html"
+  }
+];
