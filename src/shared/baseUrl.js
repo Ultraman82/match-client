f6ce3a97c8faeaf66d423ab9cca99d96e73eea77
Ultrman82 <exelcior99@gmail.com>
2019-10-07 20:08:39 -0700

    Readme added

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -2,4 +2,5 @@
 //export const baseUrl = "http://localhost:3000/";
 //https://ec2-18-222-35-37.us-east-2.compute.amazonaws.com:3443
 
-export const baseUrl = "https://ec2-18-222-35-37.us-east-2.compute.amazonaws.com:3443/";
+export const baseUrl =
+  "https://ec2-18-220-196-170.us-east-2.compute.amazonaws.com:3443/";
